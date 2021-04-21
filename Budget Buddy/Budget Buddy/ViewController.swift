2e970281e94a4f86c9b6b367d2f71627eede3cf5
Henry Nguyen <Henry.nguyen@stonybrook.edu>
2021-04-21 15:39:51 -0400

    Initial Version

---
+++ Budget Buddy/Budget Buddy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Budget Buddy
+//
+//  Created by Henry Nguyen on 4/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
