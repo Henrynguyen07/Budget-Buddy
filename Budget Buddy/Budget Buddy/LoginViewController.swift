eab1e03f3c7adff8e1c1d327e249e8d48dbdb4bd
jose guerra <josexjulian@hotmail.com>
2021-04-21 17:47:33 -0400

    login

---
+++ Budget Buddy/Budget Buddy/LoginViewController.swift
@@ -0,0 +1,56 @@
+//
+//  LoginViewController.swift
+//  Budget Buddy
+//
+//  Created by Jose Lopez on 4/21/21.
+//
+
+import UIKit
+import Parse
+
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func onLogin(_ sender: Any) {
+        let username = usernameField.text!
+        let password = passwordField.text!
+        PFUser.logInWithUsername(inBackground: username, password: password) { (user, error) in
+            if (user != nil){
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+            print("Error: \(String(describing: error?.localizedDescription))")
+            
+        }
+    }
+    @IBAction func onSignUp(_ sender: Any) {
+        let user = PFUser()
+        user.username = usernameField.text
+        user.password = passwordField.text
+        user.signUpInBackground { (success, error) in
+            if (success){
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }else{
+                print("Error: \(String(describing: error?.localizedDescription))")
+            }
+        }
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
