8ce7bc44700f67c566eeab635a5935bb1a2f70c2
jose guerra <josexjulian@hotmail.com>
2021-05-01 20:08:42 -0400

    Settings configuration

--- Budget Buddy/Budget Buddy/SettingsViewController.swift
+++ Budget Buddy/Budget Buddy/SettingsViewController.swift
@@ -6,16 +6,109 @@
 //
 
 import UIKit
+struct Section {
+    let title: String
+    let options: [SettingsOption]
+}
+struct SettingsOption {
+    let title: String
+    let icon: UIImage?
+    let iconBackgroundColor: UIColor
+    let handler: (() -> Void)
+}
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    
+    private let tableView : UITableView = {
+        let table = UITableView(frame: .zero, style: .grouped)
+        table.register(SettingTableViewCell.self, forCellReuseIdentifier: SettingTableViewCell.identifier)
+        return table
+    }()
+    
+    var models = [Section]()
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        configure()
+        title = "Settings"
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.frame =  view.bounds
         // Do any additional setup after loading the view.
     }
     
+    func configure(){
+        models.append(Section(title: "General", options: [
+            SettingsOption(title: "Wifi", icon: UIImage(systemName: "house"), iconBackgroundColor: .systemPink) {
+                print("tapped first cell")
+            },
+            SettingsOption(title: "Bluetooth", icon: UIImage(systemName: "bluetooth"), iconBackgroundColor: .systemPink){
+                
+            },
+            SettingsOption(title: "Airplane Mode", icon: UIImage(systemName: "airplane"), iconBackgroundColor: .systemPink){
+                
+            },
+            SettingsOption(title: "iCloud", icon: UIImage(systemName: "cloud"), iconBackgroundColor: .systemPink){
+                
+            }
+        ]))
+        models.append(Section(title: "Information", options: [
+            SettingsOption(title: "Wifi", icon: UIImage(systemName: "house"), iconBackgroundColor: .systemPink) {
+                
+            },
+            SettingsOption(title: "Bluetooth", icon: UIImage(systemName: "bluetooth"), iconBackgroundColor: .systemPink){
+                
+            },
+            SettingsOption(title: "Airplane Mode", icon: UIImage(systemName: "airplane"), iconBackgroundColor: .systemPink){
+                
+            },
+            SettingsOption(title: "iCloud", icon: UIImage(systemName: "cloud"), iconBackgroundColor: .systemPink){
+                
+            }
+        ]))
+        models.append(Section(title: "Apps", options: [
+            SettingsOption(title: "Wifi", icon: UIImage(systemName: "house"), iconBackgroundColor: .systemPink) {
+                
+            },
+            SettingsOption(title: "Bluetooth", icon: UIImage(systemName: "bluetooth"), iconBackgroundColor: .systemPink){
+                
+            },
+            SettingsOption(title: "Airplane Mode", icon: UIImage(systemName: "airplane"), iconBackgroundColor: .systemPink){
+                
+            },
+            SettingsOption(title: "iCloud", icon: UIImage(systemName: "cloud"), iconBackgroundColor: .systemPink){
+                
+            }
+        ]))
+                
+    }
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let model = models[section]
+        return model.title
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return models.count
+    }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models[section].options.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model = models[indexPath.section].options[indexPath.row]
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: SettingTableViewCell.identifier, for: indexPath) as? SettingTableViewCell else{
+            return UITableViewCell()
+        }
+        cell.configure(with: model)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let model = models[indexPath.section].options[indexPath.row]
+        model.handler()
+    }
     @IBAction func darkMode(_ sender: Any) {
         super.viewDidLoad()
         if self.traitCollection.userInterfaceStyle == .light {
---
+++ Budget Buddy/Budget Buddy/SwitchTableViewCell.swift
@@ -0,0 +1,79 @@
+//
+//  SwitchTableViewCell.swift
+//  Budget Buddy
+//
+//  Created by Jose Lopez on 5/1/21.
+//
+
+import UIKit
+
+class SwitchTableViewCell: UITableViewCell {
+
+  static let identifier = "SwitchTableViewCell"
+    private let iconContainer: UIView = {
+        let view = UIView()
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 8
+        view.layer.masksToBounds =  true
+        return view
+    }()
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 1
+        return label
+    }()
+    
+    private let myswitch: UISwitch = {
+        let myswitch = UISwitch()
+    myswitch.onTintColor = .systemBlue
+    return myswitch
+    }()
+    
+    private let iconImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.tintColor = .white
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.addSubview(label)
+        contentView.addSubview(iconContainer)
+        contentView.addSubview(myswitch)
+        iconContainer.addSubview(iconImageView)
+        contentView.clipsToBounds = true
+        accessoryType = .none
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        let size: CGFloat = contentView.frame.size.height - 12
+        iconContainer.frame = CGRect(x: 15, y: 6, width: size, height: size)
+        
+        let imageSize: CGFloat  = size/1.5
+        iconImageView.frame = CGRect(x: (size-imageSize)/2, y: (size-imageSize)/2, width: imageSize, height: imageSize)
+        myswitch.sizeToFit()
+        myswitch.frame = CGRect(x: (contentView.frame.size.width - myswitch.frame.size.width - 20), y: (contentView.frame.size.height - myswitch.frame.size.height)/2, width: myswitch.frame.size.width, height: myswitch.frame.size.height)
+        label.frame = CGRect(x: 25 + iconContainer.frame.size.width, y: 0, width: contentView.frame.size.width - 20 - iconContainer.frame.size.width, height: contentView.frame.size.height)
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        iconImageView.image = nil
+        label.text = nil
+        iconContainer.backgroundColor = nil
+        myswitch.isOn = false
+    }
+    
+    public func configure(with model: SettingsSwitchOption){
+        label.text = model.title
+        iconImageView.image = model.icon
+        iconContainer.backgroundColor = model.iconBackgroundColor
+        myswitch.isOn = model.isOn
+    }
+
+}
